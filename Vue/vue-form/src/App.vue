e90396a9e3ec3c5fd4ae402b33c7a854a7bc69d9
minhee <queen.minhee@gmail.com>
2020-04-26 21:34:50 +0900

    [Update] Toast Popup

--- Vue/vue-form/src/App.vue
+++ Vue/vue-form/src/App.vue
@@ -17,12 +17,17 @@
       </div>
       <button v-bind:disabled="!isUsernameValid" type="submit">로그인</button>
     </form>
-    <p v-if="isError">올바르지 않은 ID 입니다.</p>
-    <p v-if="isUsernameValid">이메일 형식이 맞습니다.</p>
+    <p v-if="isSuccess">로그인이 되었습니다.</p>
+    <!-- <p v-if="isError">올바르지 않은 ID 입니다.</p>
+    <p v-if="isUsernameValid">이메일 형식이 맞습니다.</p>-->
+    <!-- <toast-popup></toast-popup> -->
+    <ToastPopup v-bind:open="isSuccess" v-on:close="isSuccess =false"></ToastPopup>
   </div>
 </template>
 
 <script>
+import ToastPopup from "@/components/ToastPopup.vue";
+
 //이메일 형식 체크 함수
 function validateEmail(email) {
   // eslint 때문에 정규식에 에러가 발생해서 참죄: https://github.com/eslint/eslint/issues/6148
@@ -31,12 +36,17 @@ function validateEmail(email) {
 }
 
 export default {
+  components: {
+    // 'ToastPopup':ToastPopup
+    ToastPopup //ES6 에서 Object의 key, value가 같으면 literal
+  },
   data() {
     return {
       // key: value
       username: "",
       password: "",
-      isError: false
+      isError: false,
+      isSuccess: false
     };
   },
   computed: {
@@ -57,7 +67,8 @@ export default {
       //   .catch(error => {
       //     this.isError = true;
       //   });
-      this.isError = true;
+      this.isSuccess = true;
+      // this.isError = true;
       // this.initForm();
     },
     initForm() {
---
+++ Vue/vue-form/src/components/ToastPopup.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="toast" v-bind:class="{'animation':open}">로그인이 완료되었습니다.</div>
+</template>
+
+<script>
+let timer;
+
+export default {
+  props: ["open"],
+  watch: {
+    // props 속성중 open 이 변할 때마다 동작.
+    open(newValue) {
+      if (newValue) {
+        console.log("토스트 팝업 열기");
+
+        //clearTimeout => brwoser stack(메모리)에 쌓이지 않도록
+        clearTimeout(timer);
+        timer = setTimeout(() => this.$emit("close"), 2000);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.toast {
+  width: 200px;
+  height: 40px;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: -5%;
+  left: 5%;
+  transition: bottom 0.5s linear;
+}
+.animation {
+  bottom: 5%;
+}
+</style>
